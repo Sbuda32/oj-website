a4544973f7ec2c0a23a203143bbb2b67dd558580
sbuda32 <sbuda32@gmail.com>
2019-05-27 20:43:34 +0200

    Modified oj.js, oj.html, index.css, and index.html, to make the side preload with a rotating skull

--- js/oj.js
+++ js/oj.js
@@ -1,2 +1,20 @@
+(function () {
 
+    let preload = document.getElementById('preloader');
+    let loading = 0;
+    let id = setInterval( frame, 64 );
+
+    function frame () {
+        if( loading === 100 ) {
+            clearInterval(id);
+            window.open('../oj.html', '_self');
+        }
+        else {
+            loading += 1;
+            if( loading == 90 ) {
+                preload.style.animation = 'fading 1s ease';
+            }
+        }
+    }
+})();
   
